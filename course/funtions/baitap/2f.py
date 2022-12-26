df0efd875781b736f69d257669e5013f0baf4321
Pham Le Huy Hoang <phamlehuyhoang84@gmail.com>
2022-12-26 09:08:48 +0700

    add

--- course/funtions/baitap/2f.py
+++ course/funtions/baitap/2f.py
@@ -4,17 +4,19 @@ def kiemtrasonguyento(n):
     while i<=n:
         if n%i==0:
             count=count+1
-        i=i+1
+        i+=1
     if count==2:
         return True
-    else:
-        return False
+
+    
+
 def tongsonguyento(n):
     i=1
     sum=0
     while i<=n:
         if kiemtrasonguyento(i)==True:
             sum=sum+i
-        print(sum)
-        return sum
+        i+=1
+    print(sum)
+    return sum
 tongsonguyento(9)
--- course/funtions/baitap/2g.py
+++ course/funtions/baitap/2g.py
@@ -1,21 +1,23 @@
 import math
 def kiemtrasochinhphuong(n):
     i=1
+    i==math.sqrt(n)
     while i<=n:
-        i==math.sqrt(n)
         if i*i==n:
             return True
-        else:
-            return False
+        i+=1
+
+        
+        
 def tongsochinhphuong(n):
     i=1
     sum=0
     while i<=n:
         if kiemtrasochinhphuong(i)==True:
             sum=sum+i
-        print(sum)
-        return sum
-
+        i=i+1
+    print(sum)
+    return sum
 tongsochinhphuong(9)
 
 
